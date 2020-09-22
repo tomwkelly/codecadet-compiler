c8026202e05d277c4c388bc58f960efa229fc014
Tom Kelly <tom-kelly@hotmail.co.uk>
2020-09-22 12:09:03 +0100

    attempt to fix automerge

--- .circleci/auto-merge.sh
+++ .circleci/auto-merge.sh
@@ -10,17 +10,13 @@ m=$(curl \
 -H "Authorization: token ${2}" \
   https://api.github.com/repos/tomwkelly/codecadet-compiler/commits/${1} |  grep -o '"message": "[^"]*' | grep -o '[^"]*$')
 
-n=$(curl \
-  -X POST \
-  -H "Accept: application/vnd.github.v3+json" \
--H "Authorization: token ${2}"  https://api.github.com/repos/tomwkelly/codecadet-compiler/pulls \
-  -d "{\"title\":\"${m}\",\"head\":\"${1}\",\"base\":\"master\"}" | grep '"number": [^,]*' | grep -o '[0-9]\d*')
+echo m
 
-http_code=$(curl \
-  -X PUT \
-  -H "Accept: application/vnd.github.v3+json" \
--H "Authorization: token ${2}" https://api.github.com/repos/tomwkelly/codecadet-compiler/pulls/$n/merge \
-  -d "{\"commit_title\":\"Merge ${1} into master\"}" -w %{response_code} -o /dev/null )
+n=$(curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${2}" https://api.github.com/repos/tomwkelly/codecadet-compiler/pulls -d "{\"title\":\"${m}\",\"head\":\"${1}\",\"base\":\"master\"}" | grep '"number": [^,]*' | grep -o '[0-9]\d*')
+
+echo n
+
+http_code=$(curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${2}" https://api.github.com/repos/tomwkelly/codecadet-compiler/pulls/$n/merge -d "{\"commit_title\":\"Merge ${1} into master\"}" -s -w %{response_code} -o /dev/null )
 
 retrycount=0
 
@@ -31,9 +27,5 @@ do
   echo "${http_code} retrying, attempt: ${retrycount}"
   sleep 5s
   $retrycount=$(($retrycount + 1))
-  http_code=$(curl \
-  -X PUT \
-  -H "Accept: application/vnd.github.v3+json" \
-  -H "Authorization: token ${2}" https://api.github.com/repos/tomwkelly/codecadet-compiler/pulls/$n/merge \
-  -d "{\"commit_title\":\"Merge ${1} into master\"}" -w %{response_code} -o /dev/null )
+  http_code=$(curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${2}" https://api.github.com/repos/tomwkelly/codecadet-compiler/pulls/$n/merge -d "{\"commit_title\":\"Merge ${1} into master\"}" -s -w %{response_code} -o /dev/null )
 done
