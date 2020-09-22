e4597e0381a6fcd05c5e33371c556e1071a93e8f
Tom Kelly <tom-kelly@hotmail.co.uk>
2020-09-22 12:36:00 +0100

    attempt to fix automerge

--- .circleci/auto-merge.sh
+++ .circleci/auto-merge.sh
@@ -6,15 +6,15 @@ echo $1
 
 m=$(curl \
   -X GET \
-  -H "Accept: application/vnd.github.v3+json" \
+  -H "Accept: application/vnd.github.v3+json" -s \
 -H "Authorization: token ${2}" \
   https://api.github.com/repos/tomwkelly/codecadet-compiler/commits/${1} |  grep -o '"message": "[^"]*' | grep -o '[^"]*$')
 
-echo $m
+echo "message is: $m"
 
-n=$(curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${2}" https://api.github.com/repos/tomwkelly/codecadet-compiler/pulls -d "{\"title\":\"${m}\",\"head\":\"${1}\",\"base\":\"master\"}" | grep '"number": [^,]*' | grep -o '[0-9]\d*')
+n=$(curl -X POST -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${2}" https://api.github.com/repos/tomwkelly/codecadet-compiler/pulls -d "{\"title\":\"${m}\",\"head\":\"${1}\",\"base\":\"master\"}" | grep '"number": [^,]*' | grep -o '[0-9]\d*')
 
-echo $n
+echo "number is: $n"
 
 http_code=$(curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${2}" https://api.github.com/repos/tomwkelly/codecadet-compiler/pulls/$n/merge -d "{\"commit_title\":\"Merge ${1} into master\"}" -s -w %{response_code} -o /dev/null )
 
