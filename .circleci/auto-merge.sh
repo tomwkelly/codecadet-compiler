9965aff40cc00b2c4758c8676582ae4bf524c35a
Tom Kelly <tom-kelly@hotmail.co.uk>
2020-09-22 12:24:35 +0100

    attempt to fix automerge

--- .circleci/auto-merge.sh
+++ .circleci/auto-merge.sh
@@ -22,10 +22,12 @@ retrycount=0
 
 echo $http_code
 
+echo $2
+
 while ((($http_code == 404 || $http_code == 000 || $http_code == 404000) && $retrycount < 6))
 do
   echo "${http_code} retrying, attempt: ${retrycount}"
   sleep 5s
-  $retrycount=$(($retrycount + 1))
+  $retrycount=$((retrycount + 1))
   http_code=$(curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${2}" https://api.github.com/repos/tomwkelly/codecadet-compiler/pulls/$n/merge -d "{\"commit_title\":\"Merge ${1} into master\"}" -s -w %{response_code} -o /dev/null )
 done
