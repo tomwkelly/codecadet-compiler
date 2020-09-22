cc77f45698b7f08443eddfdcba6a8778444c120e
Tom Kelly <tom-kelly@hotmail.co.uk>
2020-09-22 12:13:19 +0100

    attempt to fix automerge

--- .circleci/auto-merge.sh
+++ .circleci/auto-merge.sh
@@ -10,11 +10,11 @@ m=$(curl \
 -H "Authorization: token ${2}" \
   https://api.github.com/repos/tomwkelly/codecadet-compiler/commits/${1} |  grep -o '"message": "[^"]*' | grep -o '[^"]*$')
 
-echo m
+echo $m
 
 n=$(curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${2}" https://api.github.com/repos/tomwkelly/codecadet-compiler/pulls -d "{\"title\":\"${m}\",\"head\":\"${1}\",\"base\":\"master\"}" | grep '"number": [^,]*' | grep -o '[0-9]\d*')
 
-echo n
+echo $n
 
 http_code=$(curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${2}" https://api.github.com/repos/tomwkelly/codecadet-compiler/pulls/$n/merge -d "{\"commit_title\":\"Merge ${1} into master\"}" -s -w %{response_code} -o /dev/null )
 
@@ -22,7 +22,7 @@ retrycount=0
 
 echo $http_code
 
-while ((($http_code == 404 || $http_code == 000) && $retrycount < 6))
+while ((($http_code == 404 || $http_code == 000 || $http_code=404000) && $retrycount < 6))
 do
   echo "${http_code} retrying, attempt: ${retrycount}"
   sleep 5s
