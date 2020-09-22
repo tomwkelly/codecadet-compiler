1b53ddfa464debb73a2775b4c04e1aa0de248f2b
Tom Kelly <tom-kelly@hotmail.co.uk>
2020-09-22 15:51:50 +0100

    Commit Message Fixes: Clean up and final changes

--- .circleci/auto-merge.sh
+++ .circleci/auto-merge.sh
@@ -17,16 +17,14 @@ m=$(echo ${ADDR[1]} | cut -c 1-)
 
 echo "message is: $m"
 
-c=$(echo ${ADDR[0]} | tr '[:upper:]' '[:lower:]')
-
 echo "title is $c"
 
 
-n=$(curl -X POST -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${2}" https://api.github.com/repos/tomwkelly/codecadet-compiler/pulls -d "{\"title\":\"${c}\",\"head\":\"${1}\",\"base\":\"master\"}" | grep -o '"number": [^,]*' | grep -o '[0-9]*')
+n=$(curl -X POST -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${2}" https://api.github.com/repos/tomwkelly/codecadet-compiler/pulls -d "{\"title\":\"${m}\",\"head\":\"${1}\",\"base\":\"master\"}" | grep -o '"number": [^,]*' | grep -o '[0-9]*')
 
 echo "number is: $n"
 
-http_code=$(curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${2}" https://api.github.com/repos/tomwkelly/codecadet-compiler/pulls/$n/merge -d "{\"commit_title\":\"${m}\"}" -s -w %{response_code} -o /dev/null )
+http_code=$(curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${2}" https://api.github.com/repos/tomwkelly/codecadet-compiler/pulls/$n/merge -d "{\"commit_title\":\"${c}\"}" -s -w %{response_code} -o /dev/null )
 
 retrycount=0
 
@@ -39,5 +37,5 @@ do
   echo "${http_code} retrying, attempt: ${retrycount}"
   sleep 5s
   ((retrycount += 1))
-  http_code=$(curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${2}" https://api.github.com/repos/tomwkelly/codecadet-compiler/pulls/$n/merge -d "{\"commit_title\":\"${m}\"}" -s -w %{response_code} -o /dev/null )
+  http_code=$(curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${2}" https://api.github.com/repos/tomwkelly/codecadet-compiler/pulls/$n/merge -d "{\"commit_title\":\"${c}\"}" -s -w %{response_code} -o /dev/null )
 done
